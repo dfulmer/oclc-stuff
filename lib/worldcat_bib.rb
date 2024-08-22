904a6f83550d34894029548d728086989f62ea97
David Fulmer <dfulmer@umich.edu>
2024-08-22 09:42:14 -0400

    feat: updating the WorldcatBib class

--- lib/worldcat_bib.rb
+++ lib/worldcat_bib.rb
@@ -2,20 +2,27 @@ require "byebug"
 class WorldcatBib
   attr_reader :bib
   def self.for(oclc_num)
-    wskey = ENV.fetch("WORLDCAT_API_KEY")
+    # wskey = ENV.fetch("WORLDCAT_API_KEY")
 
-    # Connect to the Worldcat API
-    connection = Faraday.new
+    # # Connect to the Worldcat API
+    # connection = Faraday.new
 
-    # Retrieve the OCLC record with the cross reference file OCLC number given
-    response = connection.get do |req|
-      req.url "https://worldcat.org/webservices/catalog/content/#{oclc_num}?servicelevel=full&wskey=#{wskey}"
-    end
+    # # Retrieve the OCLC record with the cross reference file OCLC number given
+    # response = connection.get do |req|
+    #   req.url "https://worldcat.org/webservices/catalog/content/#{oclc_num}?servicelevel=full&wskey=#{wskey}"
+    # end
+
+    # # raising error because this really shouldn't happen.
+    # raise StandardError, "Failed to fetch bib from Worldcat" if response.status != 200
+
+    # WorldcatBib.new(response.body)
+    # Here is the new stuff for dealing with the WorldCat Metadata API version 2.0
+    result = `python3 aimworldcat4.py #{oclc_num}`
 
     # raising error because this really shouldn't happen.
-    raise StandardError, "Failed to fetch bib from Worldcat" if response.status != 200
+    raise StandardError, "Failed to fetch bib from Worldcat" if result == "Error\n"
 
-    WorldcatBib.new(response.body)
+    WorldcatBib.new(result)
   end
 
   # @param bib [String] Marc xml string from Worldcat API
